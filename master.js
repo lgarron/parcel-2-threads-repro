88f2dcd21673818dcd638052f1664e72790d1b67
Lucas Garron <code@garron.net>
2021-01-29 18:09:58 -0800

    Update example.

--- master.js
+++ master.js
@@ -4,7 +4,7 @@ import { spawn, Thread, Worker } from "threads";
 // We wrap the sample code in an async IIFE because Parcel 2's Babel defaults
 // don't support top-level await.
 (async () => {
-  const auth = await spawn(new Worker("./workers/auth"));
+  const auth = await spawn(new Worker("./workers/auth.js"));
   const hashed = await auth.hashPassword("Super secret password", "1234");
 
   console.log("Hashed password:", hashed);
